9eacd25f278acdb23a2c78186c17457d21931869
Your Name <you@example.com>
2021-11-21 16:41:55 +0100

    musiiiiiik

---
+++ myMusik/prerequisites.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get -y install udisks2
+sudo apt-get -y install libudisks2-dev
+sudo apt-get -y install build-essential
+sudo apt-get -y install libxml2-dev
+sudo apt-get -y install libdbus-1-dev
+sudo apt-get -y install libudisks2-dev
+sudo apt-get -y install libglib2.0-dev
+sudo apt-get -y install libpam0g-dev
+sudo apt-get -y install python3-pip || true
+
+
+
+
+
